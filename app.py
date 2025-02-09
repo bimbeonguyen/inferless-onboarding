f8251ca7ad44048511d3675955407e36ca86b283
bimbeonguyen <longnguyen160@gmail.com>
2025-02-09 23:51:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from transformers import AutoTokenizer
 
 class InferlessPythonModel:
   def initialize(self):
-    model_id = "rbgo/inferless-llama-3-8B"
+    model_id = "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B"
     self.llm = LLM(model=model_id,gpu_memory_utilization=0.9,max_model_len=5000,dtype="float16")
     self.tokenizer = AutoTokenizer.from_pretrained(model_id)
 
