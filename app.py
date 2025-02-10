ea62f97de384106087a9e11aa15fbc9cfb7c970b
bimbeonguyen <longnguyen160@gmail.com>
2025-02-10 23:55:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from vllm import LLM
 from vllm.sampling_params import SamplingParams
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 
 class InferlessPythonModel:
   def initialize(self):
     model_id = "unsloth/DeepSeek-R1-Distill-Qwen-7B-unsloth-bnb-4bit"
-    self.llm = LLM(model=model_id, gpu_memory_utilization=0.9, max_model_len=5000, dtype=torch.float16, quantization="bitsandbytes", load_format="bitsandbytes")
+    # self.llm = LLM(model=model_id, gpu_memory_utilization=0.9, max_model_len=5000, dtype=torch.float16, quantization="bitsandbytes", load_format="bitsandbytes")
+    self.llm = AutoModelForCausalLM.from_pretrained(model_id, load_in_8bit=True, device_map="auto")
     self.tokenizer = AutoTokenizer.from_pretrained(model_id)
 
   def infer(self, inputs):
