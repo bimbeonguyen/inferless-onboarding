ce03c5f4f62b1569932e6e6f2824262c22bbb9ed
bimbeonguyen <longnguyen160@gmail.com>
2025-02-11 09:49:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ class InferlessPythonModel:
   def initialize(self):
     model_id = "unsloth/DeepSeek-R1-Distill-Qwen-7B-unsloth-bnb-4bit"
     # self.llm = LLM(model=model_id, gpu_memory_utilization=0.9, max_model_len=5000, dtype=torch.float16, quantization="bitsandbytes", load_format="bitsandbytes")
-    self.llm = AutoModelForCausalLM.from_pretrained(model_id, load_in_8bit=True, device_map="auto")
+    self.llm = AutoModelForCausalLM.from_pretrained(model_id, load_in_4bit=True, device_map="auto")
     self.tokenizer = AutoTokenizer.from_pretrained(model_id)
 
   def infer(self, inputs):
@@ -14,7 +14,7 @@ class InferlessPythonModel:
     top_p = inputs.get("top_p",0.1)
     repetition_penalty = inputs.get("repetition_penalty",1.18)
     top_k = int(inputs.get("top_k",40))
-    max_tokens = inputs.get("max_tokens",256)
+    max_tokens = inputs.get("max_tokens",2048)
 
     inputs = self.tokenizer(prompts, return_tensors="pt").to("cuda")
     outputs = self.llm.generate(
